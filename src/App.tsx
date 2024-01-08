60fa60adb32e9743d7c93bc7fa17d4738c624dac
christian_reynaltt <creynaltt@maristak.net>
2024-01-08 21:44:26 +0100

    SALIDA de urgencia

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import { Button } from 'keep-react';
+import responseMovies from './mocks/movies.json';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [movies, setMovies] = React.useState(responseMovies.Search);
+  const hasMovies = movies?.length > 0;
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+        <img src={reactLogo} className="logo react" alt="React logo" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Buscador de películas</h1>
+
+      {hasMovies ? (
+        <ul>
+          {movies.map((item) => (
+            <li key={item.imdbID}>
+              <h3>{item.Title}</h3>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <span>'no hay nada'</span>
+      )}
+      <Button size="md">Default</Button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
